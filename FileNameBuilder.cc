f56905f343dfc6d3cb9ab698c4aea60db1902728
Andy Hawkins <andy@gently.org.uk>
2006-11-28 17:36:21 +0000

    Extra 'invalid' characters for file names
    git-svn-id: file:///tmp/flactag-git-move/svnroot/trunk@81 c94211a6-d224-44d1-9852-295abbaa3a11

--- FileNameBuilder.cc
+++ FileNameBuilder.cc
@@ -92,7 +92,7 @@ std::string CFileNameBuilder::FixString(const std::string& String) const
 	else
 		perror("unac_string");
 		
-	std::string BadChars="/:\"'`;?";
+	std::string BadChars="/:\"'`;?&,";
 		
 	for (std::string::size_type count=0;count<Fixed.length();count++)
 	{
