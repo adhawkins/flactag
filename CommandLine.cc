5d69cf0f19ef153f5ce45873b98427bcae0b9582
Andy Hawkins <andy@gently.org.uk>
2006-12-15 10:46:25 +0000

    Fix ID tags
    git-svn-id: file:///tmp/flactag-git-move/svnroot/trunk@100 c94211a6-d224-44d1-9852-295abbaa3a11

--- CommandLine.cc
+++ CommandLine.cc
@@ -20,7 +20,7 @@
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-     $Id: FileNameBuilder.cc 93 2006-12-13 13:12:28Z andy $
+     $Id$
 
 ----------------------------------------------------------------------------*/
 
