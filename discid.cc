dca4e962a4614d375f049a22d0d1beb623d65c26
Andy Hawkins <andy@gently.org.uk>
2007-01-10 23:14:16 +0000

    Modified to use libdiscid and DiscIDWrapper class
    git-svn-id: file:///tmp/flactag-git-move/svnroot/trunk@109 c94211a6-d224-44d1-9852-295abbaa3a11

--- discid.cc
+++ discid.cc
@@ -26,28 +26,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "musicbrainz/musicbrainz.h"
+
+#include "DiscIDWrapper.h"
 
 int main(int argc, const char *argv[])
 {
 	if (argc==2)
 	{
-    MusicBrainz o;
+		CDiscIDWrapper DiscID;
 
-    o.SetDevice(argv[1]);
-    //o.SetDebug(1);
-    
-    if (o.Query(string(MBQ_GetCDTOC)))
+    if (DiscID.FromDevice(argv[1]))
     {
-    	std::string RDF;
-    	printf("%s\n",o.Data(MBE_TOCGetCDIndexId).c_str());
+    	printf("%s\n",DiscID.ID().c_str());
+    	printf("%s\n",DiscID.SubmitURL().c_str());
     }
     else
     {
-    	std::string Error;
-    		
-			o.GetQueryError(Error);
-			printf("Query failed: %s\n", Error.c_str());
+			printf("Query failed: %s\n",DiscID.Error().c_str());
 		}
 	}
 	else
