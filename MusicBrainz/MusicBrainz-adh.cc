8d213fce3bf7ad9b0445468242393af078b5eb28
Andy Hawkins <andy@gently.org.uk>
2011-06-08 09:25:00 +0000

    Error check XML parsing
    git-svn-id: file:///tmp/flactag-git-move/svnroot/branches/andyh/adh-musicbrainz@242 c94211a6-d224-44d1-9852-295abbaa3a11

--- MusicBrainz/MusicBrainz-adh.cc
+++ MusicBrainz/MusicBrainz-adh.cc
@@ -16,52 +16,66 @@ MusicBrainzADH::CGenericList<MusicBrainzADH::CRelease> MusicBrainzADH::CMusicBra
 {
 	//Will this work soon (and return disc IDs as well)?
 	//http://www.musicbrainz.org/ws/2/discid/arIS30RPWowvwNEqsqdDnZzDGhk-?inc=artists+labels+recordings+release-groups+artist-credits
-	
+
 	MusicBrainzADH::CGenericList<MusicBrainzADH::CRelease> ReleaseList;
-	
+
 	std::stringstream os;
 	os << "/ws/2/discid/" << DiscID;
-	
+
 	CHTTPFetch Fetch("www.musicbrainz.org");
 
-	int Ret=Fetch.Fetch(os.str());	
+	int Ret=Fetch.Fetch(os.str());
 	if (Ret>0)
 	{
 		std::vector<unsigned char> Data=Fetch.Data();
 
-		XMLNode TopNode=XMLNode::parseString((const char *)&Data[0]);			
-		XMLNode MetadataNode=TopNode.getChildNode("metadata");
-		CMetadata Metadata(MetadataNode);
-		
-		CDisc *Disc=Metadata.Disc();
-		if (Disc && Disc->ReleaseList())
-			ReleaseList=*Disc->ReleaseList();
+		XMLResults Results;
+		XMLNode TopNode=XMLNode::parseString((const char *)&Data[0], 0, &Results);
+		if (Results.error==eXMLErrorNone)
+		{
+			XMLNode MetadataNode=TopNode.getChildNode("metadata");
+			if (!MetadataNode.isEmpty())
+			{
+				CMetadata Metadata(MetadataNode);
+
+				CDisc *Disc=Metadata.Disc();
+				if (Disc && Disc->ReleaseList())
+					ReleaseList=*Disc->ReleaseList();
+			}
+		}
 	}
-	
+
 	return ReleaseList;
-}		
+}
 
 MusicBrainzADH::CRelease MusicBrainzADH::CMusicBrainzADH::LookupRelease(const std::string& ReleaseID)
 {
 	MusicBrainzADH::CRelease Release;
-	
+
 	std::stringstream os;
 	os << "/ws/2/release/" << ReleaseID << "?inc=artists+labels+recordings+release-groups+url-rels+discids+artist-credits";
 
 	CHTTPFetch Fetch("www.musicbrainz.org");
 
-	int Ret=Fetch.Fetch(os.str());	
-		
+	int Ret=Fetch.Fetch(os.str());
+
 	if (Ret>0)
 	{
 		std::vector<unsigned char> Data=Fetch.Data();
 
-		XMLNode TopNode=XMLNode::parseString((const char *)&Data[0]);			
-		XMLNode MetadataNode=TopNode.getChildNode("metadata");
-		CMetadata Metadata(MetadataNode);
-		if (Metadata.Release())
-			Release=*Metadata.Release();
+		XMLResults Results;
+		XMLNode TopNode=XMLNode::parseString((const char *)&Data[0], 0, &Results);
+		if (Results.error==eXMLErrorNone)
+		{
+			XMLNode MetadataNode=TopNode.getChildNode("metadata");
+			if (!MetadataNode.isEmpty())
+			{
+				CMetadata Metadata(MetadataNode);
+				if (Metadata.Release())
+					Release=*Metadata.Release();
+			}
+		}
 	}
-		
+
 	return Release;
-}		
+}
