b24801d8849e693c06ab838a706cb666d0246fb5
Andy Hawkins <andy@gently.org.uk>
2011-06-09 12:29:10 +0000

    Code tidying, split things into helper functions to make it more readable
    git-svn-id: file:///tmp/flactag-git-move/svnroot/branches/andyh/adh-musicbrainz@245 c94211a6-d224-44d1-9852-295abbaa3a11

--- MusicBrainzInfo.h
+++ MusicBrainzInfo.h
@@ -4,18 +4,18 @@
    						using data retrieved from the MusicBrainz service
 
    Copyright (C) 2006 Andrew Hawkins
-   
+
    This file is part of flactag.
-   
+
    Flactag is free software; you can redistribute it and/or
    modify it under the terms of v2 of the GNU Lesser General Public
    License as published by the Free Software Foundation.
-   
+
    Flactag is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-   
+
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -32,18 +32,27 @@
 #include "Album.h"
 #include "Cuesheet.h"
 
+#include "MusicBrainz/Release.h"
+#include "MusicBrainz/Medium.h"
+#include "MusicBrainz/Track.h"
+#include "MusicBrainz/ArtistCredit.h"
+
 class CMusicBrainzInfo
 {
 public:
 	CMusicBrainzInfo(const std::string& Server, const CCuesheet& Cuesheet);
-	
+
 	bool LoadInfo(const std::string& FlacFile);
 	std::vector<CAlbum> Albums() const;
-	
+
 private:
 	std::string AlbumType(const std::string Type) const;
 	std::string AlbumStatus(const std::string Status) const;
 	void WaitRequest() const;
+	CAlbum ParseAlbum(const MusicBrainzADH::CRelease& Release, const MusicBrainzADH::CMedium& Medium);
+	CTrack ParseTrack(const MusicBrainzADH::CTrack& MBTrack);
+	void ParseArtist(const MusicBrainzADH::CArtistCredit* ArtistCredit, std::string& ArtistID, std::string& ArtistName, std::string& ArtistSort);
+	std::vector<unsigned char> GetCoverArt(const CUTF8Tag& ASIN);
 
 	std::vector<CAlbum> m_Albums;
 	std::string m_Server;
