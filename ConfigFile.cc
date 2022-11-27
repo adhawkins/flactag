5cd3e0e893d66f6d367013791858992cb8ca1411
Andy Hawkins <andy@gently.org.uk>
2022-11-27 23:55:48 +0000

    Update copyright notice

--- ConfigFile.cc
+++ ConfigFile.cc
@@ -1,28 +1,28 @@
 /* --------------------------------------------------------------------------
 
-   flactag -- A tagger for single album FLAC files with embedded CUE sheets
-   						using data retrieved from the MusicBrainz service
+	 flactag -- A tagger for single album FLAC files with embedded CUE sheets
+							using data retrieved from the MusicBrainz service
 
-   Copyright (C) 2006-2012 Andrew Hawkins
-   Copyright (C) 2011-2012 Daniel Pocock
+	 Copyright (C) 2006-2022 Andrew Hawkins
+	 Copyright (C) 2011-2012 Daniel Pocock
 
-   This file is part of flactag.
+	 This file is part of flactag.
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
+	 This program is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
 	 the Free Software Foundation, either version 3 of the License, or
 	 (at your option) any later version.
 
-   Flactag is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+	 Flactag is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	 Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	 You should have received a copy of the GNU General Public
+	 License along with this library; if not, write to the Free Software
+	 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-     $Id$
+		 $Id$
 
 ----------------------------------------------------------------------------*/
 
